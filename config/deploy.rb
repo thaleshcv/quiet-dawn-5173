14f8a6b5ac143bc1dcb23b75020dad0716109a5a
thaleshcv <thalesherbert@gmail.com>
2021-01-18 23:56:36 -0300

    updating credentials/deploy for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,9 @@ set :default_env, {
 append :linked_files, "config/master.key", "config/credentials/production.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
 
+Rake::Task["deploy:assets:backup_manifest"].clear_actions
+Rake::Task["deploy:assets:restore_manifest"].clear_actions
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
