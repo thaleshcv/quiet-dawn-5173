928137c176522900e37e70fe73a8f20cfead8c76
thaleshcv <thalesherbert@gmail.com>
2021-02-18 10:57:19 -0300

    Fixing Self-Update script

--- bin/self_update.sh
+++ bin/self_update.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-APP_HOME="${HOME}/quiet-dawn-5173"
+APP_HOME="/home/ubuntu/quiet-dawn-5173"
 
-if [[ "${RAILS_ENV}" != "production" ]]; then
-  echo "Production-Only Script. Leaving...";
-  exit 0
-fi;
+if [[ -d $APP_HOME ]]; then
+  echo "APP_HOME found... Let's go..."
+else
+  echo "APP_HOME NOT found... Leaving..."
+  exit 1
+fi
 
+echo "=== ENTERING ${APP_HOME} ==="
 cd $APP_HOME
 git pull
 
 echo "=== RUNNING MIGRATIONS ==="
-bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake db:migrate
 
 echo "=== PRECOMPILING ASSETS ==="
-bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake assets:precompile
 
 echo "=== RESTARTING PASSENGER ==="
 passenger-config restart-app .
