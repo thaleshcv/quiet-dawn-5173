28387414c7e2c3d60155a1a182bb3a87af1168b3
thaleshcv <thalesherbert@gmail.com>
2021-02-25 22:19:40 -0300

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,10 @@ require("turbolinks").start();
 require("jquery");
 require("chartkick");
 require("chart.js");
+require("popper.js");
+
+import "bootstrap/js/dist/util";
+import "bootstrap/js/dist/dropdown";
 
 import "../setup";
-
 import "../stylesheets/application";
