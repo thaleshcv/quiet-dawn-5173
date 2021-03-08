659e218e657624d2d4c61359acfdadfd9c981e0b
thaleshcv <thalesherbert@gmail.com>
2021-03-08 09:36:24 -0300

    Refactoring and moving 'chart_range_option' helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def chart_range_option(label, path, range:, disabled: false)
+    button_tag(label,
+      type: "button",
+      disabled: disabled,
+      class: "btn btn-secondary btn-sm range-#{range}",
+      data: {
+        remote: true,
+        url: path,
+        method: "GET"
+      })
+  end
+
   def difference_in_percent(initial, current)
     # avoiding division by zero and NaN values
     difference = if initial.to_f.zero? || current.to_f.zero?
--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,13 +1,2 @@
 module AssetsHelper
-  def chart_range_option(asset, label, range:, disabled: false)
-    button_tag(label,
-      type: "button",
-      disabled: disabled,
-      class: "btn btn-secondary btn-sm range-#{range}",
-      data: {
-        remote: true,
-        url: accumulated_asset_url(asset, range_type: range),
-        method: "GET"
-      })
-  end
 end
