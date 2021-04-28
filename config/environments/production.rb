c5f199c53db654edb2cc24991755ea59d5f7d28e
thaleshcv <thalesherbert@gmail.com>
2021-04-28 15:39:24 -0300

    Update mailer configuration in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # ActionMailer configuration
-  config.action_mailer.default_url_options = { host: "rykko.tk" }
-
+  config.action_mailer.default_url_options = { host: "rykko.tk", protocol: "https" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
