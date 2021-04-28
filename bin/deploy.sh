19df03dcdeed06e770ccd721abf26a3fcad30ebc
thaleshcv <thalesherbert@gmail.com>
2021-04-28 12:40:42 -0300

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,11 +4,18 @@ APP_HOME="/home/ubuntu/quiet-dawn-5173"
 
 ssh ubuntu@150.136.55.99 "bash --login -c 'cd ${APP_HOME} && git pull && bin/self_update.sh'"
 
-echo
-echo "====== TO UPDATE THE ASSETS, DO THE FOLLOWING: ======="
-echo
-echo "rm -rf public/packs"
-echo "RAILS_ENV=production bundle exec rake assets:precompile"
-echo "scp -pr public/ ubuntu@150.136.55.99:${APP_HOME}/"
-echo "ssh ubuntu@150.136.55.99 \"bash --login -c 'cd ${APP_HOME} && passenger-config restart-app .'\""
-echo "rm -rf public/packs"
+echo "==== UPDATED ====="
+
+read -p "Update packs? [y|N] " packs
+
+if [[ $packs = "y" || $packs = "Y" ]]; then
+  echo "====== UPDATING PACKS ======="
+  rm -rf public/packs
+  RAILS_ENV=production bundle exec rake assets:precompile
+  scp -pr public/packs ubuntu@150.136.55.99:${APP_HOME}/public
+  ssh ubuntu@150.136.55.99 "bash --login -c 'cd ${APP_HOME} && passenger-config restart-app .'"
+  rm -rf public/packs
+  bin/yarn install --check-files
+fi
+
+echo "==== DEPLOY FINISHED!!! ===="
